a36083cda61acca53d39aa6ce3a7667bfe7b47e0
Chris Ferone <chrisferone@gmail.com>
2023-09-06 22:44:48 -0400

    Add comments, ROI

--- solar_roi.py
+++ solar_roi.py
@@ -1,5 +1,8 @@
-# Calculate return on investment of solar panels. Assume no power is used by 
-# home. All power is sold back to grid. Calculate value of solar energy.
+# Calculate return on investment (ROI) of solar panels. 
+
+# Assume no power is used by home. All power is sold back to grid. Calculate 
+# value of annual solar energy production. Divide cost of solar panel 
+# installation by annual solar energy production income to get ROI.
 
 import pandas as pd
 import numpy as np
@@ -9,9 +12,9 @@ import numpy as np
 # https://newlook.dteenergy.com/wps/wcm/connect/23195474-a4d1-4d38-aa30-a4426fd3336b/WholeHouseRateOptions.pdf?MOD=AJPERES
 
 class Rate:
-  def __init__(self, name, PSCR, distr):
+  def __init__(self, name, pscr, distr):
     self.name = name
-    self.PSCR = PSCR
+    self.pscr = pscr
     self.distr = distr
     self.smmr = [0, 0] # Define summer months
     self.peak = [0, 0] # Define peak hours
@@ -33,14 +36,17 @@ class Rate:
     self.wntr_offpk_buy = 0
 
   def calc_rates(self):
-    self.smmr_peak_sell = self.smmr_peak_cap + self.smmr_peak_ncp + self.PSCR
-    self.smmr_offpk_sell = self.smmr_offpk_cap + self.smmr_offpk_ncp + self.PSCR
-    self.wntr_peak_sell = self.wntr_peak_cap + self.wntr_peak_ncp + self.PSCR
-    self.wntr_offpk_sell = self.wntr_offpk_cap + self.wntr_offpk_ncp + self.PSCR
+    # Calculate cost to purchase and sell back electricity in the summer and 
+    # winter during on-peak and off-peak hours
+    self.smmr_peak_sell = self.smmr_peak_cap + self.smmr_peak_ncp + self.pscr
+    self.smmr_offpk_sell = self.smmr_offpk_cap + self.smmr_offpk_ncp + self.pscr
+    self.wntr_peak_sell = self.wntr_peak_cap + self.wntr_peak_ncp + self.pscr
+    self.wntr_offpk_sell = self.wntr_offpk_cap + self.wntr_offpk_ncp + self.pscr
     self.smmr_peak_buy = self.smmr_peak_sell + self.distr
     self.smmr_offpk_buy = self.smmr_offpk_sell + self.distr
     self.wntr_peak_buy = self.wntr_peak_sell + self.distr
     self.wntr_offpk_buy = self.wntr_offpk_sell + self.distr
+    # Print purchase rates
     print(self.name + " Purchase Rates")
     print("\tSummer Peak: %.3f" % self.smmr_peak_buy)
     print("\tSummer OffP: %.3f" % self.smmr_offpk_buy)
@@ -48,11 +54,11 @@ class Rate:
     print("\tWinter OffP: %.3f" % self.wntr_offpk_buy + "\n")
 
 # Time-invariant Costs
-PSCR = 1.917 # Power Supply Cost Recovery
+pscr = 1.917 # Power Supply Cost Recovery
 distr = 6.879 # Distributions
 
 # Time of Day 3 p.m. – 7 p.m. Standard Base Rate (D1.11) 
-D1_11 = Rate("D1_11",PSCR, distr)
+D1_11 = Rate("D1_11",pscr, distr)
 D1_11.smmr = [6, 9]
 D1_11.peak = [15, 19]
 D1_11.smmr_peak_cap = 7.941
@@ -66,7 +72,7 @@ D1_11.wntr_offpk_ncp = 3.746
 D1_11.calc_rates()
 
 # Time of Day 11 a.m. - 7 p.m. Rate (D1.2) 
-D1_2 = Rate("D1_2", PSCR, distr)
+D1_2 = Rate("D1_2", pscr, distr)
 D1_2.smmr = [6, 10]
 D1_2.peak = [11, 19]
 D1_2.smmr_peak_cap = 11.033
@@ -79,17 +85,19 @@ D1_2.wntr_offpk_cap = 0.792
 D1_2.wntr_offpk_ncp = 4.105
 D1_2.calc_rates()
 
-# import csv file generate from NREL PV_Watts Calculator
+# Import hourly solar energy generation csv file created by NREL PV_Watts Calculator
 df = pd.read_csv("pvwatts_hourly.csv", skiprows=31)
 
-# Add two empty columns to the dataframe: One to capture electric sellback rate 
-# ($/100/kWh) each hour and another to capture hourly profit
+# Add two empty columns to the dataframe for each rate option: One to capture electric 
+# sellback rate (¢/kWh) each hour and another to capture hourly profit
 df["D1_11_rate"] = np.nan
 df["D1_11_profit"] = np.nan
 df["D1_2_rate"] = np.nan
 df["D1_2_profit"] = np.nan
 
-def calc_profit(rate):
+def calc_hourly_sellback_rate(rate):
+    # Calculate the price of selling back electricity to the grid for each hour 
+    # of each day of the year
     if (month < rate.smmr[0]) or (month > rate.smmr[1]):
         # winter
         if (hour < rate.peak[0]) or (hour > rate.peak[1]):
@@ -107,16 +115,27 @@ def calc_profit(rate):
             # on peak
             df.iloc[i, df.columns.get_loc(rate.name  + "_rate")] = rate.smmr_peak_sell
 
+# Calculate the hourly electricity sellback rate for each rate option (i.e. 
+# D1.11 and D1.2)
 for i in range(0, len(df.index)):
     hour = df.iloc[i][2]
     month = df.iloc[i][0]
-    calc_profit(D1_11)
-    calc_profit(D1_2)
+    calc_hourly_sellback_rate(D1_11)
+    calc_hourly_sellback_rate(D1_2)
 
-df["D1_11_profit"] = df["D1_11_rate"] / 100 * df["AC System Output (W)"] / 1000
-df["D1_2_profit"] = df["D1_2_rate"] / 100 * df["AC System Output (W)"] / 1000
+# Get the product of the hourly energy production in kWh and the hourly sellback
+# rate in $/kWh
+df["D1_11_profit"] = df["AC System Output (W)"] / 1000 * df["D1_11_rate"] / 100
+df["D1_2_profit"] = df["AC System Output (W)"] / 1000 * df["D1_2_rate"] / 100
+D1_11_profit = df["D1_11_profit"].sum()
+D1_2_profit = df["D1_2_profit"].sum()
 
-#print(df.iloc[:48])
+# Print the results
 print("Annual profit selling all electricity back to DTE:")
-print("\t D1_11: $" + '{:0,.2f}'.format(df["D1_11_profit"].sum()))
-print("\t D1_2: $" + '{:0,.2f}'.format(df["D1_2_profit"].sum()))
+print("\t D1_11: $" + '{:0,.2f}'.format(D1_11_profit))
+print("\t D1_02: $" + '{:0,.2f}'.format(D1_2_profit))
+
+Cost = 35000
+print("\nROI (years):")
+print("\t D1_11: " + '{:0,.1f}'.format(Cost/D1_11_profit))
+print("\t D1_02: " + '{:0,.1f}'.format(Cost/D1_2_profit))
