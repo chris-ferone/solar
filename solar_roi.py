12d53e9f115e855299ce1be3ddc14ac73fa969bd
Chris Ferone <chrisferone@gmail.com>
2023-09-06 21:36:28 -0400

    Calc profit for both rate schedules

--- solar_roi.py
+++ solar_roi.py
@@ -9,7 +9,8 @@ import numpy as np
 # https://newlook.dteenergy.com/wps/wcm/connect/23195474-a4d1-4d38-aa30-a4426fd3336b/WholeHouseRateOptions.pdf?MOD=AJPERES
 
 class Rate:
-  def __init__(self, PSCR, distr):
+  def __init__(self, name, PSCR, distr):
+    self.name = name
     self.PSCR = PSCR
     self.distr = distr
     self.smmr = [0, 0] # Define summer months
@@ -46,7 +47,7 @@ PSCR = 1.917 # Power Supply Cost Recovery
 distr = 6.879 # Distributions
 
 # Time of Day 3 p.m. – 7 p.m. Standard Base Rate (D1.11) 
-D1_11 = Rate(PSCR, distr)
+D1_11 = Rate("D1_11",PSCR, distr)
 D1_11.smmr = [6, 9]
 D1_11.peak = [15, 19]
 D1_11.smmr_peak_cap = 7.941
@@ -60,7 +61,7 @@ D1_11.wntr_offpk_ncp = 3.746
 D1_11.calc_rates()
 
 # Time of Day 11 a.m. - 7 p.m. Rate (D1.2) 
-D1_2 = Rate(PSCR, distr)
+D1_2 = Rate("D1_2", PSCR, distr)
 D1_2.smmr = [6, 10]
 D1_2.peak = [11, 19]
 D1_2.smmr_peak_cap = 11.033
@@ -78,35 +79,38 @@ df = pd.read_csv("pvwatts_hourly.csv", skiprows=31)
 
 # Add two empty columns to the dataframe: One to capture electric sellback rate 
 # ($/100/kWh) each hour and another to capture hourly profit
-df["rate"] = np.nan
-df["profit"] = np.nan
+df["D1_11_rate"] = np.nan
+df["D1_11_profit"] = np.nan
+df["D1_2_rate"] = np.nan
+df["D1_2_profit"] = np.nan
 
 def calc_profit(rate):
     if (month < rate.smmr[0]) or (month > rate.smmr[1]):
         # winter
         if (hour < rate.peak[0]) or (hour > rate.peak[1]):
             # off peak
-            df.iloc[i, df.columns.get_loc("rate")] = rate.wntr_offpk_sell
+            df.iloc[i, df.columns.get_loc(rate.name + "_rate")] = rate.wntr_offpk_sell
         else:
             # on peak
-            df.iloc[i, df.columns.get_loc("rate")] = rate.wntr_peak_sell
+            df.iloc[i, df.columns.get_loc(rate.name  + "_rate")] = rate.wntr_peak_sell
     else:
         # summer
         if (hour < rate.peak[0]) or (hour > rate.peak[1]):
             # off peak
-            df.iloc[i, df.columns.get_loc("rate")] = rate.smmr_offpk_sell
+            df.iloc[i, df.columns.get_loc(rate.name  + "_rate")] = rate.smmr_offpk_sell
         else:
             # on peak
-            df.iloc[i, df.columns.get_loc("rate")] = rate.smmr_peak_sell
+            df.iloc[i, df.columns.get_loc(rate.name  + "_rate")] = rate.smmr_peak_sell
 
-# D1.2
 for i in range(0, len(df.index)):
     hour = df.iloc[i][2]
     month = df.iloc[i][0]
+    calc_profit(D1_11)
     calc_profit(D1_2)
 
-df["profit"] = df["rate"] / 100 * df["AC System Output (W)"] / 1000
+df["D1_11_profit"] = df["D1_11_rate"] / 100 * df["AC System Output (W)"] / 1000
+df["D1_2_profit"] = df["D1_2_rate"] / 100 * df["AC System Output (W)"] / 1000
 
 #print(df.iloc[:48])
-
-print(df["profit"].sum())
+print(df["D1_11_profit"].sum())
+print(df["D1_2_profit"].sum())
