0ab3c7d389c51896cd1321a07749a3bd9cf2d604
Chris Ferone <chrisferone@gmail.com>
2023-09-06 20:13:46 -0400

    Clean up code

--- solar_roi.py
+++ solar_roi.py
@@ -1,29 +1,19 @@
-# Calculate return on investment of solar panels
+# Calculate return on investment of solar panels. Assume no power is used by 
+# home. All power is sold back to grid. Calculate value of solar energy.
 
 import pandas as pd
 import numpy as np
 
-
-
 # import csv file generate from NREL PV_Watts Calculator
-df = pd.read_csv('pvwatts_hourly.csv', skiprows = 31)
+df = pd.read_csv("pvwatts_hourly.csv", skiprows=31)
 
-
-# Assume no power is used by home. All power is sold back to grid. Calculate value of solar energy.
-
-# D1.11
-
-# D1.2
-
-
-# Create rate vector, which ultimately will be multiplied by power cost vector
+# Add two empty columns to the dataframe: One to capture electric sellback rate 
+# ($/100/kWh) each hour and another to capture hourly profit
 df["rate"] = np.nan
 df["profit"] = np.nan
 
-# Each element of rate vector will have one of 4 possible values
-
 # D1.2
-for i in range(0,len(df.index)):
+for i in range(0, len(df.index)):
     hour = df.iloc[i][2]
     month = df.iloc[i][0]
     if (month < 6) or (month > 10):
@@ -43,17 +33,8 @@ for i in range(0,len(df.index)):
             # on peak
             df.iloc[i, df.columns.get_loc("rate")] = 17.055
 
+df["profit"] = df["rate"] / 100 * df["AC System Output (W)"] / 1000
 
-
-# Loop through each month of the year, then each day, then each hour.
-#for i in range()
-#for i in range(0,len(df.index)):
-for i in range(0,24):
-    # print(df.iloc[0])
-    print(df.iloc[i][12])
-
-df['profit'] = df['rate']/100*df['AC System Output (W)']/1000
-
-print(df.iloc[:48])
+#print(df.iloc[:48])
 
 print(df["profit"].sum())
