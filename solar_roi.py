e847a7ea601657e0b8c06296c4842847a3b9b833
Chris Ferone <chrisferone@gmail.com>
2023-09-06 20:13:46 -0400

    Create price column in df

--- solar_roi.py
+++ solar_roi.py
@@ -1,12 +1,13 @@
 # Calculate return on investment of solar panels
 
 import pandas as pd
+import numpy as np
 
 
 
 # import csv file generate from NREL PV_Watts Calculator
 df = pd.read_csv('pvwatts_hourly.csv', skiprows = 31)
-print(df)
+
 
 # Assume no power is used by home. All power is sold back to grid. Calculate value of solar energy.
 
@@ -14,13 +15,39 @@ print(df)
 
 # D1.2
 
+
+# Create price vector, which ultimately will be multiplied by power cost vector
+df["price"] = np.nan
+
+# Each element of price vector will have one of 4 possible values
+
+# D1.2
+for i in range(0,len(df.index)):
+    hour = df.iloc[i][2]
+    month = df.iloc[i][0]
+    if (month < 6) or (month > 10):
+        # winter
+        if (hour < 11) or (hour > 19):
+            # off peak
+            df.iloc[i, df.columns.get_loc("price")] = 6.814
+        else:
+            # on peak
+            df.iloc[i, df.columns.get_loc("price")] = 14.704
+    else:
+        # summer
+        if (hour < 11) or (hour > 19):
+            # off peak
+            df.iloc[i, df.columns.get_loc("price")] = 7.013
+        else:
+            # on peak
+            df.iloc[i, df.columns.get_loc("price")] = 17.055
+
+print(df)
+
+
 # Loop through each month of the year, then each day, then each hour.
 #for i in range()
 #for i in range(0,len(df.index)):
 for i in range(0,24):
     # print(df.iloc[0])
-    print(df.iloc[i][11]/1000)
-
-# Create price vector, which ultimately will be multiplied by power vector
-# Price vector will have 4 values
-
+    print(df.iloc[i][12]/1000)
