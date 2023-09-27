11fc083c24771279adcc8838bb49dc778bb78fca
Chris Ferone <chrisferone@gmail.com>
2023-09-26 21:50:19 -0400

    Calc payback period for D1_8 rate

--- solar_roi.py
+++ solar_roi.py
@@ -38,6 +38,8 @@ class Rate:
             if rate['name'] == name:
                 self.name = rate['name']              
                 if name == 'D1_8':
+                    self.peak = rate['peak_hours']
+                    self.mdpeak = rate['mdpeak_hours']
                     self.peak_sell = rate['peak_cap'] + rate['peak_ncp'] + pscr
                     self.midpk_sell = rate['midpk_cap'] + rate['midpk_ncp'] + pscr
                     self.offpk_sell = rate['offpk_cap'] + rate['offpk_ncp'] + pscr
@@ -79,43 +81,64 @@ D1_8 = Rate("D1_8", elctr_rates)
 # Import hourly solar energy generation csv file created by NREL PV_Watts Calculator
 df = pd.read_csv("pvwatts_hourly.csv", skiprows=31)
 
-# Add two empty columns to the dataframe for each rate option: One to capture electric
-# sellback rate (¢/kWh) each hour and another to capture hourly profit
+# Add three empty columns to the dataframe for each rate option: One to capture 
+# electric sellback rate (¢/kWh) each hour, another to capture hourly profit ($) 
+# if electricity is sold back, another to capture savings if electricity is used 
+# (i.e. cost averted)
 df["D1_11_sell_rate"] = np.nan
 df["D1_11_profit"] = np.nan
 df["D1_11_cost"] = np.nan
 df["D1_2_sell_rate"] = np.nan
 df["D1_2_profit"] = np.nan
 df["D1_2_cost"] = np.nan
+df["D1_8_sell_rate"] = np.nan
+df["D1_8_profit"] = np.nan
+df["D1_8_cost"] = np.nan
 
 def calc_hourly_sellback_rate(rate):
     # Calculate the price of selling back electricity to the grid for each hour
     # of each day of the year
-    if (month < rate.smmr[0]) or (month > rate.smmr[1]):
-        # winter
-        if (hour < rate.peak[0]) or (hour > rate.peak[1]):
-            # off peak
-            df.iloc[
-                i, df.columns.get_loc(rate.name + "_sell_rate")
-            ] = rate.wntr_offpk_sell
+    if rate.name != 'D1_8':
+        if (month < rate.smmr[0]) or (month > rate.smmr[1]):
+            # winter
+            if (hour < rate.peak[0]) or (hour > rate.peak[1]):
+                # off peak
+                df.iloc[
+                    i, df.columns.get_loc(rate.name + "_sell_rate")
+                ] = rate.wntr_offpk_sell
+            else:
+                # on peak
+                df.iloc[
+                    i, df.columns.get_loc(rate.name + "_sell_rate")
+                ] = rate.wntr_peak_sell
         else:
-            # on peak
-            df.iloc[
-                i, df.columns.get_loc(rate.name + "_sell_rate")
-            ] = rate.wntr_peak_sell
+            # summer
+            if (hour < rate.peak[0]) or (hour > rate.peak[1]):
+                # off peak
+                df.iloc[
+                    i, df.columns.get_loc(rate.name + "_sell_rate")
+                ] = rate.smmr_offpk_sell
+            else:
+                # on peak
+                df.iloc[
+                    i, df.columns.get_loc(rate.name + "_sell_rate")
+                ] = rate.smmr_peak_sell
     else:
-        # summer
-        if (hour < rate.peak[0]) or (hour > rate.peak[1]):
-            # off peak
-            df.iloc[
-                i, df.columns.get_loc(rate.name + "_sell_rate")
-            ] = rate.smmr_offpk_sell
-        else:
-            # on peak
-            df.iloc[
-                i, df.columns.get_loc(rate.name + "_sell_rate")
-            ] = rate.smmr_peak_sell
-
+            if (hour >= rate.peak[0]) and (hour < rate.peak[1]):
+                # on peak
+                df.iloc[
+                    i, df.columns.get_loc(rate.name + "_sell_rate")
+                ] = rate.offpk_sell
+            if ((hour >= rate.mdpeak[0][0]) and (hour < rate.mdpeak[0][1]) or (hour >= rate.mdpeak[1][0]) and (hour < rate.mdpeak[1][1])):
+                # mid peak
+                 df.iloc[
+                    i, df.columns.get_loc(rate.name + "_sell_rate")
+                ] = rate.midpk_sell
+            else:
+                # off peak
+                df.iloc[
+                    i, df.columns.get_loc(rate.name + "_sell_rate")
+                ] = rate.peak_sell
 
 # Calculate the hourly electricity sellback rate for each rate option (i.e.
 # D1.11 and D1.2)
@@ -124,13 +147,16 @@ for i in range(0, len(df.index)):
     month = df.iloc[i][0]
     calc_hourly_sellback_rate(D1_11)
     calc_hourly_sellback_rate(D1_2)
+    calc_hourly_sellback_rate(D1_8)
 
 # Get the product of the hourly energy production in kWh and the hourly sellback
 # rate in $/kWh
 df["D1_11_profit"] = df["AC System Output (W)"] / 1000 * df["D1_11_sell_rate"] / 100
 df["D1_2_profit"] = df["AC System Output (W)"] / 1000 * df["D1_2_sell_rate"] / 100
+df["D1_8_profit"] = df["AC System Output (W)"] / 1000 * df["D1_8_sell_rate"] / 100
 D1_11_profit = df["D1_11_profit"].sum()
 D1_2_profit = df["D1_2_profit"].sum()
+D1_8_profit = df["D1_8_profit"].sum()
 
 # Get the product of the hourly energy production in kWh and the hourly purchase
 # rate in $/kWh
@@ -140,24 +166,32 @@ df["D1_11_cost"] = (
 df["D1_2_cost"] = (
     df["AC System Output (W)"] / 1000 * (df["D1_2_sell_rate"] + D1_2.distr) / 100
 )
+df["D1_8_cost"] = (
+    df["AC System Output (W)"] / 1000 * (df["D1_8_sell_rate"] + D1_8.distr) / 100
+)
 D1_11_value = df["D1_11_cost"].sum()
 D1_2_value = df["D1_2_cost"].sum()
+D1_8_value = df["D1_8_cost"].sum()
 
 # Print the results
 print("Annual profit selling all electricity back to DTE:")
 print("\t D1_11: $" + "{:0,.2f}".format(D1_11_profit))
 print("\t D1_02: $" + "{:0,.2f}".format(D1_2_profit))
+print("\t D1_08: $" + "{:0,.2f}".format(D1_8_profit))
 
 Cost = 35000
 print("\nPayback Period (years):")
 print("\t D1_11: " + "{:0,.1f}".format(Cost / D1_11_profit))
 print("\t D1_02: " + "{:0,.1f}".format(Cost / D1_2_profit))
+print("\t D1_08: " + "{:0,.1f}".format(Cost / D1_8_profit))
 
 # Print the results
 print("\nAnnual savings selling zero electricity back to DTE:")
 print("\t D1_11: $" + "{:0,.2f}".format(D1_11_value))
 print("\t D1_02: $" + "{:0,.2f}".format(D1_2_value))
+print("\t D1_08: $" + "{:0,.2f}".format(D1_8_value))
 
 print("\nPayback Period (years):")
 print("\t D1_11: " + "{:0,.1f}".format(Cost / D1_11_value))
 print("\t D1_02: " + "{:0,.1f}".format(Cost / D1_2_value))
+print("\t D1_08: " + "{:0,.1f}".format(Cost / D1_8_value))
